13485ef3e4ee2ddeed8ee7eb0bd98e6df2c32ac2
ntwigamartin <ntwiga.martin@gmail.com>
2023-01-20 13:44:00 +0300

    Completed

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -2,7 +2,22 @@ import React from "react";
 import { actors } from "../data";
 
 function Actors() {
-  return <div>{/*{code here}*/}</div>;
+    
+return (
+  <div>
+    <h1>Actors Page</h1>
+    {actors.map((actor, index) => {
+  return (
+      <div key={index}>
+        {actor.name}
+        <ul>
+          {actor.movies.map((movie, index) => <li key={'Act_'+index}>{movie}</li>)}
+        </ul>
+      </div>
+  )
+})}
+  </div>
+)
 }
 
 export default Actors;
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,7 +2,21 @@ import React from "react";
 import { movies } from "../data";
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+   
+  return (
+      <div>
+      <h1>Movies Page</h1>
+      {movies.map((movie, index) => (
+        <div key={index}>
+          {movie.title}<br/>
+          {movie.time}
+          <ul>
+            {movie.genres.map((genre, index) => (<li key={'Genre_'+ index}>{genre}</li>))}
+          </ul>
+        </div>
+  ))}
+      </div>
+  )
 }
 
 export default Movies;
